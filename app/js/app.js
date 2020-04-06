5e5df3340c92bbae2b0f06369d63fa5a76693a75
Wiss69 <wissem.cheriguene@gmail.com>
2020-04-06 11:03:29 +0200

    V2.0

--- app/js/app.js
+++ app/js/app.js
@@ -1,196 +1,142 @@
-let app = {
+var app = {
 
-  apiBaseUrl: 'https://api.covid19api.com/',
+  init: function() {
+    // console.log('init');
 
+    // Fonction qui charge les pays
+      app.loadCountryList();
 
-    init: function() {
-        console.log('coucou !');
+    // Fonction qui gere les ecouteurs d'event
+    app.initEventListeners();
+  },
 
-        // LOAD COUNTRIES
-        app.loadCountries();
-
-        // Event listeners
-        app.eventListeners();
+  loadCountryList: function() {
     
-    },
+    app.showOverlay();
 
-    eventListeners: function() {
-        
-             // Fermer modal
-             const seeInfosModal = document.querySelector('.SeeInfosModal');
-             const returnButtons = seeInfosModal.querySelectorAll('.return');
-            //  console.log(returnButtons);
-             returnButtons.forEach(button => button.addEventListener('click', app.closeSeeInfosModal));
-        
-    },
+    axios.get('https://api.covid19api.com/summary')
+    .then(function(response) {
+      response.data.Countries.forEach(app.handleDisplayCountries);
+      console.log(response.data.Global);
+      app.handleGlobalInfos(response.data.Global);
+      app.hideOverlay();
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  },
 
-    loadCountries: function() {
-        // charge tous les pays
-        axios.get(app.apiBaseUrl + 'countries')
-        .then(function(response) {
-            // console.log(response.data);
-            response.data.forEach(app.displayCountries);
-            // response.data.forEach(app.confirmedCasesByCountry);
-        })
-        .catch(function(error) {
-            console.log(error)
-        });
-    },
-        
-    // Display countries
-    displayCountries: function(country) {
-        
-        // console.log(country);
-        if(country.Country != ''){
-            const myList = document.getElementById('content');
-            const button = myList.querySelector('button');
-            var buttonNode = button.cloneNode(true);
-            buttonNode.dataset.slug =  country.Slug;
-            buttonNode.innerHTML = country.Country;
-            myList.appendChild(buttonNode);
-        }
+  handleGlobalInfos: function(global) {
+    console.log(global);
+    const infosElement = document.querySelector('.globalInfos');
+    const newConfirmed = infosElement.querySelector('.NewConfirmed');
+    const newDeaths = infosElement.querySelector('.NewDeaths');
+    const newRecovered = infosElement.querySelector('.NewRecovered');
+    const totalConfirmed = infosElement.querySelector('.TotalConfirmed');
+    const totalDeaths = infosElement.querySelector('.TotalDeaths');
+    const totalRecovered = infosElement.querySelector('.TotalRecovered');
 
-        buttonNode.addEventListener('click', app.handleDisplayData);
+    newConfirmed.textContent = global.NewConfirmed;
+    newDeaths.textContent = global.NewDeaths;
+    newRecovered.textContent = global.NewRecovered;
+    totalConfirmed.textContent = global.TotalConfirmed;
+    totalDeaths.textContent = global.TotalDeaths;
+    totalRecovered.textContent = global.TotalRecovered;
+    // console.log(newConfirmed);
+  },
 
-    },
+  handleDisplayCountries: function(data) {
 
-    handleDisplayData: function(e) {
+    // console.log(data);
 
-        e.preventDefault();
-        // console.log(e.target.dataset.slug);
-        const countrySlug = e.target.dataset.slug;
+    // On récupère le template d'un élément de liste de recette
+    const countryListItemTemplate = document.getElementById('country-list-item-template');
+    // console.log(countryListItemTemplate);
 
-        //https://api.covid19api.com/dayone/country/south-africa/status/confirmed
-
-        let confirmed = 'https://api.covid19api.com/dayone/country/' + countrySlug + '/status/confirmed';
-        let recovered = 'https://api.covid19api.com/dayone/country/' + countrySlug + '/status/recovered';
-        let deaths = 'https://api.covid19api.com/dayone/country/' + countrySlug + '/status/deaths';
-
-        const confirmedRequest = axios.get(confirmed);
-        const recoveredRequest = axios.get(recovered);
-        const deathsRequest = axios.get(deaths);
-
-        axios
-            .all([confirmedRequest, recoveredRequest, deathsRequest])
-            .then(
-                axios.spread((confirmed, recovered, deaths) => {
-                    // return confirmed, recovered, deaths; 
-                    app.fillSeeInfosModal(confirmed.data, recovered.data, deaths.data);                   
-                })
-            )
-            // Don't work if I send datas with then ???
-            // .then(app.fillSeeInfosModal(confirmed, recovered, deaths))
-
-            // open the modal
-            .then(app.openSeeInfosModal)
-            .catch(function(error) {
-                console.log(error);
-            });      
-    },
-
-    fillSeeInfosModal: function (confirmed, recovered, deaths) {
-
-        console.log('confirmed : ', confirmed);
-        console.log('recovered : ', recovered);
-        console.log('deaths : ',  deaths);
-
-        if (confirmed.length > 0 && recovered.length > 0 && deaths.length > 0) {
-
-            // Array last element
-            const lastElementInConfirmedArray = confirmed[confirmed.length - 1];
-            const lastElementInRecoveredArray = recovered[recovered.length - 1];
-            const lastElementInDeathsArray = deaths[deaths.length - 1];
-            
-            // // // Cases Value
-            const casesConfirmed = lastElementInConfirmedArray.Cases;
-            const casesRecovered = lastElementInRecoveredArray.Cases;
-            const casesDeaths = lastElementInDeathsArray.Cases;
-            
-            console.log('confirmed : ', casesConfirmed);
-            console.log('recovered : ', casesRecovered);
-            console.log('deaths : ',  casesDeaths);
-
-            // Select modal element
-            const SeeInfosModal = document.querySelector('.SeeInfosModal');
-
-            // Select each fields
-            const confirmedCasesElement = SeeInfosModal.querySelector('.case-confirmed');
-            const deathCasesElement = SeeInfosModal.querySelector('.case-deaths');
-            const recoveredCasesElement = SeeInfosModal.querySelector('.case-recovered');
-            
-            // Filling each fields
-              
-            
-
-            var maxConfirmedCases = 0;
-            
-            // foreach sur tableau
-            confirmed.forEach(function(data) {
+    // Get the content
+    const countryListItemTemplateContent = countryListItemTemplate.content;
     
-                // console.log(data.Cases)
-                var caseNumber = data.Cases;
+    // Clone the first child
+    const countryListItemTemplateContentElement = countryListItemTemplateContent.firstElementChild;
+    const countryListItem = countryListItemTemplateContentElement.cloneNode(true);
     
-                if (caseNumber > maxConfirmedCases) {
-                    maxConfirmedCases = data.Cases;
-                }
-            });
-            console.log(maxConfirmedCases);
-            confirmedCasesElement.textContent = maxConfirmedCases; 
+    // console.log(countryListItem);
+    const title = countryListItem.querySelector('.countryTitle');
+    title.textContent = data.Country;
+    countryListItem.dataset.Date = data.Date;
+    countryListItem.dataset.Country = data.Country;
+    countryListItem.dataset.NewConfirmed = data.NewConfirmed;
+    countryListItem.dataset.NewDeaths = data.NewDeaths;
+    countryListItem.dataset.NewRecovered = data.NewRecovered;
+    countryListItem.dataset.TotalConfirmed = data.TotalConfirmed;
+    countryListItem.dataset.TotalDeaths = data.TotalDeaths;
+    countryListItem.dataset.TotalRecovered = data.TotalRecovered;
 
-            var maxDeathsCases = 0;
-            deaths.forEach(function(data) {
-                if(data.Cases > maxDeathsCases) {
-                    maxDeathsCases = data.Cases;
-                }
-            })
-            console.log(maxDeathsCases);
-            deathCasesElement.textContent = maxDeathsCases;
+    // Fill the seemodal
 
-            var maxRecoveredCases = 0;
-            deaths.forEach(function(data) {
-                if(data.Cases > maxRecoveredCases) {
-                    maxRecoveredCases = data.Cases;
-                }
-            })
-            console.log(maxRecoveredCases);
-            recoveredCasesElement.textContent = maxRecoveredCases;
 
-        }
+
+    // Add it to the array
+    const countryList = document.querySelector('.countries-list');
+    countryList.appendChild(countryListItem);
+
+    const showButton = countryListItem.querySelector('.showcountry');
+    showButton.addEventListener('click', app.fillSeeModal);
+
+  },
+
+  openShowCountry: function() {
+    document.querySelector('.seeCountryModal').classList.add('is-active');
+  },
+  
+  initEventListeners: function() {
+    const closeButtons = document.querySelectorAll('.closeModal');
+    closeButtons.forEach(button => button.addEventListener('click', app.closeModal));
+  },
+  
+  closeModal: function() {
+    document.querySelector('.seeCountryModal').classList.remove('is-active');
+  },
+
+  fillSeeModal: function(e) {
+
+    // seeCountryModal
+    const countryElement = e.target.closest('.country');
+    const seeCountryModal = document.querySelector('.seeCountryModal');
+    
+        const countryListItemTitle = seeCountryModal.querySelector('.country');
+        const countryListItemDate = seeCountryModal.querySelector('.date');
+        const countryListItemNewConfirmed = seeCountryModal.querySelector('.newConfirmed');
+        const countryListItemNewDeaths = seeCountryModal.querySelector('.newDeaths');
+        const countryListItemNewRecovered = seeCountryModal.querySelector('.newRecovered');
+        const countryListItemTotalConfirmed = seeCountryModal.querySelector('.totalConfirmed');
+        const countryListItemTotalDeaths = seeCountryModal.querySelector('.totalDeaths');
+        const countryListItemTotalRecovered = seeCountryModal.querySelector('.totalRecovered');
         
-        if(confirmed.length == 0) {
-            // Select modal element
-            const SeeInfosModal = document.querySelector('.SeeInfosModal');
-            // Select each fields
-            const confirmedCasesElement = SeeInfosModal.querySelector('.case-confirmed');
-            // Fill fields with 0
-            confirmedCasesElement.textContent = '0';   
-        }
-        if(recovered.length == 0) {
-            // Select modal element
-            const SeeInfosModal = document.querySelector('.SeeInfosModal');
-            // Select each fields
-            const recoveredCasesElement = SeeInfosModal.querySelector('.case-recovered');
-            // Fill fields with 0
-            recoveredCasesElement.textContent = '0';
-        }
-        if(deaths.length == 0) {
-            // Select modal element
-            const SeeInfosModal = document.querySelector('.SeeInfosModal');
-            // Select each fields
-            const deathsCasesElement = SeeInfosModal.querySelector('.case-deaths');
-            // Fill fields with 0
-            deathsCasesElement.textContent = '0';
-        }
-        
-    },
+        countryListItemDate.textContent = countryElement.dataset.Date;
+        countryListItemTitle.textContent = countryElement.dataset.Country;
+        countryListItemNewConfirmed.textContent = countryElement.dataset.NewConfirmed;
+        countryListItemNewDeaths.textContent = countryElement.dataset.NewDeaths;
+        countryListItemNewRecovered.textContent = countryElement.dataset.NewRecovered;
+        countryListItemTotalConfirmed.textContent = countryElement.dataset.TotalConfirmed;
+        countryListItemTotalDeaths.textContent = countryElement.dataset.TotalDeaths;
+        countryListItemTotalRecovered.textContent = countryElement.dataset.TotalRecovered;
 
-    openSeeInfosModal: function() {
-        document.querySelector('.SeeInfosModal').classList.add('is-active');
-    },
+        app.openShowCountry();
+  },
 
-    closeSeeInfosModal: function() {
-        document.querySelector('.SeeInfosModal').classList.remove('is-active');
-    }
+  showOverlay: function() {
+    console.log('Chargement en cours...');
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.remove('is-hidden');
+  },
+
+  hideOverlay: function() {
+    console.log('Chargement terminé !');
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.add('is-hidden');
+  }
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
+
